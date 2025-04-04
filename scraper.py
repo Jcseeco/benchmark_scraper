7fb1907330ac646268865f4cf1e67a4be9f7320e
Jcseeco <dontbirdanyone@gmail.com>
2025-04-04 12:26:54 -0700

    feat: extract line score

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import datetime
 import json
 from os import path,makedirs
 import pandas as pd
+from functools import reduce
 
 OUTPUT_DIR = "./outputs"
 GAMEID_FILE = "gameIds.csv"
@@ -28,41 +29,60 @@ def append_json(file_name:str, data:list):
         json.dump(new_data,file,indent=4)        
         
 
-def MLB_play_by_play(game_id):
+def MLB_play_by_play(driver, game_id):
+    """returns play-by-play script in one string
+    """
+    print(f"extracting play-by-play: {game_id}")
     play_url = "https://www.espn.com/mlb/playbyplay/_/gameId/" + game_id
     play_class_name = "PlayHeader__description"
     
-    # Set up the Selenium WebDriver
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Run without opening a browser
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service,options=options)
+    driver.get(play_url)
+    time.sleep(2)  # Wait for the page to load (adjust as needed)
     
-    try:
-        driver.get(play_url)
-        time.sleep(2)  # Wait for the page to load (adjust as needed)
+    # Find elements by class name
+    divs = driver.find_elements(By.CLASS_NAME, play_class_name)
+    
+    # Extract and join into one string
+    joined_text = ""
+    for div in divs:
+        if not div.text.strip():
+            continue
         
-        # Find elements by class name
-        divs = driver.find_elements(By.CLASS_NAME, play_class_name)
-        
-        # Extract and join into one string
-        joined_text = ""
-        for div in divs:
-            if not div.text.strip():
-                continue
+        # add period to end of pitching sentence
+        if "PITCHING" in div.text:
+            joined_text += div.text +". "
+        else:
+            joined_text += div.text +" "
             
-            # add period to end of pitching sentence
-            if "PITCHING" in div.text:
-                joined_text += div.text +". "
-            else:
-                joined_text += div.text +" "
-                
-        return joined_text
+    return joined_text
         
-    finally:
-        driver.quit()
-def mlb_line_score(url):
-    raise NotImplementedError
+def mlb_line_score(driver, game_id):
+    """returns line score table in markdown format
+    """
+    print(f"extracting linescore: {game_id}")
+    play_url = "https://www.espn.com/mlb/playbyplay/_/gameId/" + game_id
+    
+    driver.get(play_url)
+    time.sleep(2)  # Wait for the page to load (adjust as needed)
+    
+    # Find elements by class name
+    team_table = driver.find_element(By.CSS_SELECTOR, "div.LineScore table:first-of-type")
+    team1 = team_table.find_element(By.CSS_SELECTOR,"tr:nth-of-type(2) a.AnchorLink:nth-child(2)").text
+    team2 = team_table.find_element(By.CSS_SELECTOR,"tr:nth-of-type(3) a.AnchorLink:nth-child(2)").text
+    
+    score_table = driver.find_element(By.CSS_SELECTOR,"div.LineScore div.Table__ScrollerWrapper table")
+    team1_scores = score_table.find_elements(By.CSS_SELECTOR,"tbody tr:nth-child(1) td:nth-of-type(-n+9)")
+    team2_scores = score_table.find_elements(By.CSS_SELECTOR,"tbody tr:nth-child(1) td:nth-of-type(-n+9)")
+    
+    md_table = "| Teams | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |\n| - | - | - | - | - | - | - | - | - | - |\n"
+    team1_row = f"| {team1} | "
+    team2_row = f"| {team2} | "
+    for i in range(9):
+        team1_row += team1_scores[i].text + ' | '
+        team2_row += team2_scores[i].text + ' | '
+    
+    return md_table+team1_row+'\n'+team2_row
+        
 
 def mlb_play_n_score():
     """ 
@@ -83,14 +103,24 @@ def mlb_play_n_score():
     # create output directory if not exists 
     makedirs(OUTPUT_DIR ,exist_ok=True)
     
+    # Set up the Selenium WebDriver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Run without opening a browser
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service,options=options)
+    
     # scrape data
     new_data = []
     for game_id in game_ids:
-        transcript = MLB_play_by_play(game_id)
+        transcript = MLB_play_by_play(driver, game_id)
+        line_score = mlb_line_score(driver,game_id)
         new_data.append({
             "game_id": game_id,
-            "input": transcript
+            "input": transcript,
+            "output": line_score
         })
+    
+    driver.close()
     
     # write to output file
     append_json(out_file,new_data)
@@ -99,7 +129,7 @@ def mlb_play_n_score():
 if __name__ == "__main__":
 
     func_code = input("select func:\n"
-          "1. MLB play-by-play and scoreboard\n"
+          "1. MLB play-by-play and linescore\n"
           "2. MLB play-by-play and boxscore\n")
     
     if func_code == "1":
